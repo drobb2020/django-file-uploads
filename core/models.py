43b9a4d0e903b1c5392aeb17ff53aafb13ff8382
David Robb <66834033+drobb2020@users.noreply.github.com>
2023-04-01 12:02:54 -0400

    Added file validation

--- core/models.py
+++ core/models.py
@@ -1,9 +1,24 @@
+import magic
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
 from django.db import models
+
+ext_validator = FileExtensionValidator(["png", "jpg", "pdf"])
+
+
+def validate_file_mimetype(file):
+    accept = ["image/png", "image/jpeg", "application/pdf"]
+    file_mime_type = magic.from_buffer(file.read(1024), mime=True)
+    print(file_mime_type)
+    if file_mime_type not in accept:
+        raise ValidationError("Unsupported file type")
 
 
 class Dog(models.Model):
     name = models.CharField(max_length=64)
-    image = models.ImageField(upload_to='dogs/')
+    image = models.FileField(
+        upload_to="dogs/", validators=[ext_validator, validate_file_mimetype]
+    )
 
     def __str__(self):
         return self.name
